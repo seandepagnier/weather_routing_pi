d6af1875360961c85cc78771246368133f515c7e
Sean D'Epagnier <seandepagnier@gmail.com>
2016-08-11 12:50:35 -0400

    hopefully fix the osx problem in a nicer way

--- src/Utilities.h
+++ src/Utilities.h
@@ -40,7 +40,7 @@
 # define INFINITY std::numeric_limits<double>::infinity ()
 # endif
 
-#define isnan _isnan
+//#define isnan _isnan
 #define isinf(x) (!_finite(x) && !_isnan(x))
 
 inline double trunc(double d){ return (d>0) ? floor(d) : ceil(d) ; }
@@ -60,6 +60,8 @@ inline double round(double n) { return n < 0.0 ? ceil(n - 0.5) : floor(n + 0.5);
 char *strtok_r(char *str, const char *delim, char **save);
 #endif
 
+#include <cmath>
+using std::isnan;
 
 /* min must have correct paren to make predence correct */
 #ifdef MIN
