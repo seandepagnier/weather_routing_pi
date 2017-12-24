32ac00184d1a88a70b29c569e9d91085d9495678
didier <dgautheron@magic.fr>
2017-12-24 11:17:51 +0100

    bump plugin API to 115 for new grib records format

--- src/weather_routing_pi.h
+++ src/weather_routing_pi.h
@@ -87,7 +87,7 @@ std::cout << x << std::endl; } while (0)
 
 class WeatherRouting;
 
-class weather_routing_pi : public wxEvtHandler, public opencpn_plugin_113
+class weather_routing_pi : public wxEvtHandler, public opencpn_plugin_115
 {
 public:
       weather_routing_pi(void *ppimgr);
