a9b9f8e838c48f0e807600f5229a9568f849d859
Sean D'Epagnier <seandepagnier@gmail.com>
2018-01-03 19:00:34 -0500

    hopefully correct clang error (and bug)

--- src/Polar.cpp
+++ src/Polar.cpp
@@ -181,7 +181,7 @@ Polar::Polar()
 
 #define MAX_WINDSPEEDS_IN_TABLE 200
 #define MESSAGE(S) (S + wxString(_T("\n")) + wxString::FromUTF8(filename) \
-                    + (line > 0 ? (_(" line ") + wxString::Format(_T("%d"), linenum)) : _T("")))
+                    + (linenum > 0 ? (_(" line ") + wxString::Format(_T("%d"), linenum)) : _T("")))
 #define PARSE_WARNING(S) do { if(message.empty()) message = MESSAGE(S); } while (0)
 #define PARSE_ERROR(S) do { message = _("Boat polar failed") + wxString(_T("\n")) \
                                   + MESSAGE(S); goto failed; } while (0)
