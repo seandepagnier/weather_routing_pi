52dbea5cd4f3c93368f9623794a37c16cd5f3db9
Stelian Pop <stelian@popies.net>
2018-02-19 11:35:32 +0100

    Fix vertical alignment of many labels, and expansion of wxTimePickerCtrl

--- src/WeatherRoutingUI.h
+++ src/WeatherRoutingUI.h
@@ -1,8 +1,8 @@
 ///////////////////////////////////////////////////////////////////////////
-// C++ code generated with wxFormBuilder (version Feb 17 2018)
+// C++ code generated with wxFormBuilder (version Feb 18 2018)
 // http://www.wxformbuilder.org/
 //
-// PLEASE DO "NOT" EDIT THIS FILE!
+// PLEASE DO *NOT* EDIT THIS FILE!
 ///////////////////////////////////////////////////////////////////////////
 
 #ifndef __WEATHERROUTINGUI_H__
