a265576272d7e48051a6cc5a173b428921fe0274
Rick Gleason <fcgleason@tgp-architects.com>
2020-03-20 09:30:25 -0400

    add trusty bionic-gtk3 chng cloud-upload

---
+++ ci/circleci-build-trusty.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+#
+# Build the Trusty Ubuntu artifacts
+#
+set -xe
+sudo apt-get -qq update
+sudo apt-get install devscripts equivs gdebi
+
+rm -rf build && mkdir build && cd build
+mk-build-deps ../ci/control
+sudo gdebi -n ./*all.deb  || :
+sudo apt-get --allow-unauthenticated install -f
+rm -f ./*all.deb
+
+tag=$(git tag --contains HEAD)
+
+if [ -n "$BUILD_GTK3" ]; then
+  sudo update-alternatives --set wx-config /usr/lib/*-linux-*/wx/config/gtk3-unicode-3.0
+fi
+
+if [ -n "$tag" ]; then
+  cmake -DCMAKE_BUILD_TYPE=Release ..
+else
+  cmake -DCMAKE_BUILD_TYPE=Debug ..
+fi
+
+make -j2
+make package
+ls -l
