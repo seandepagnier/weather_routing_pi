222a1b8e30f6973d9f008c6fd8f1fae0af3c8e74
Rick Gleason <fcgleason@tgp-architects.com>
2020-03-20 11:07:44 -0400

    fix cloudsmith-upload.sh

--- ci/cloudsmith-upload.sh
+++ ci/cloudsmith-upload.sh
@@ -3,4 +3,4 @@
 # Run created upload script
 CURRENT_DIR=$(pwd)
 # chmod a+x $CURRENT_DIR/build/cloudsmith-upload.sh
-bash $CURRENT_DIR/build/clousmith-upload.sh
+bash $CURRENT_DIR/build/cloudsmith-upload.sh
