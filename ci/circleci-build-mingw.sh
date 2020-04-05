cc1dcebd648b84d00ace7333730c2e105ee52d63
Rick Gleason <fcgleason@tgp-architects.com>
2020-04-05 10:42:16 -0400

    mingw wget download pvw32con.exe

--- ci/circleci-build-mingw.sh
+++ ci/circleci-build-mingw.sh
@@ -8,6 +8,9 @@ set -xe
 
 su -c "dnf install -q -y sudo dnf-plugins-core"
 sudo dnf builddep -y mingw/fedora/opencpn-deps.spec
+cd buildwin
+wget https://downloads.sourceforge.net/project/opencpnplugins/opencpn_packaging_data/PVW32Con.exe
+cd ..
 rm -rf build; mkdir build; cd build
 cmake -DCMAKE_TOOLCHAIN_FILE=../mingw/fedora/toolchain.cmake ..
 make -j2
