7b4b7a6b714422004e5def844332cc2e6e09f39f
Rick Gleason <fcgleason@tgp-architects.com>
2020-04-05 11:05:21 -0400

    updates to match cur process raspbian using gtk2

--- ci/travis-build-raspbian-armhf.sh
+++ ci/travis-build-raspbian-armhf.sh
@@ -29,8 +29,8 @@ DOCKER_CONTAINER_ID=$(sudo docker ps | grep raspbian | awk '{print $1}')
 docker exec -ti $DOCKER_CONTAINER_ID apt-get update
 docker exec -ti $DOCKER_CONTAINER_ID echo "------\nEND apt-get update\n" 
 
-docker exec -ti $DOCKER_CONTAINER_ID apt-get -y install git cmake build-essential cmake gettext wx-common libwxgtk3.0-dev libbz2-dev libcurl4-openssl-dev libexpat1-dev libcairo2-dev libarchive-dev liblzma-dev libexif-dev lsb-release 
-
+#docker exec -ti $DOCKER_CONTAINER_ID apt-get -y install git cmake build-essential cmake gettext wx-common libwxgtk3.0-dev libbz2-dev libcurl4-openssl-dev libexpat1-dev libcairo2-dev libarchive-dev liblzma-dev libexif-dev lsb-release 
+docker exec -ti $DOCKER_CONTAINER_ID apt-get -y install git cmake build-essential cmake gettext wx-common libgtk2.0-dev libwxgtk3.0-dev libbz2-dev libcurl4-openssl-dev libexpat1-dev libcairo2-dev libarchive-dev liblzma-dev libexif-dev lsb-release
 
 #docker exec -ti $DOCKER_CONTAINER_ID echo $OCPN_BRANCH
 
