c9bed6e7f779c5ff168c8a3be1c0129bf8c26c12
Rick Gleason <fcgleason@tgp-architects.com>
2020-05-02 11:22:17 -0400

    MacOS final fix JG

--- ci/circleci-build-macos.sh
+++ ci/circleci-build-macos.sh
@@ -6,22 +6,25 @@
 
 # Fix broken ruby on the CircleCI image:
 if [ -n "$CI" ]; then
-    curl -fsSL \
-        https://raw.githubusercontent.com/Homebrew/install/master/uninstall \
-        > uninstall
-    chmod 755 uninstall
-    ./uninstall -f
-    inst="https://raw.githubusercontent.com/Homebrew/install/master/install"
-    /usr/bin/ruby -e "$(curl -fsSL $inst)"
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 fi
-
 
 set -xe
 
+#for pkg in cairo libexif xz libarchive wget cmake; do
+#    brew list $pkg 2>/dev/null | head -10 || brew install $pkg
+#done
+#brew unlink python@2
+#brew upgrade python
+#brew ls python3
+#brew install wget
+#brew install cmake
 set -o pipefail
-for pkg in cairo libexif xz libarchive python3 wget cmake; do
-    brew list $pkg 2>/dev/null | head -10 || brew install $pkg
+for pkg in cairo cmake libarchive libexif wget; do
+    brew list $pkg 2>&1 >/dev/null || brew install $pkg 2>&1 >/dev/null || brew upgrade $pkg
 done
+brew list python@2 2>&1 >/dev/null && brew unlink python@2
+brew reinstall python3
 
 wget -q http://opencpn.navnux.org/build_deps/wx312_opencpn50_macos109.tar.xz
 tar xJf wx312_opencpn50_macos109.tar.xz -C /tmp
@@ -29,12 +32,12 @@ export PATH="/usr/local/opt/gettext/bin:$PATH"
 echo 'export PATH="/usr/local/opt/gettext/bin:$PATH"' >> ~/.bash_profile
 
 rm -rf build && mkdir build && cd build
-test -z "$TRAVIS_TAG" && CI_BUILD=OFF || CI_BUILD=ON
 cmake \
   -DwxWidgets_CONFIG_EXECUTABLE=/tmp/wx312_opencpn50_macos109/bin/wx-config \
   -DwxWidgets_CONFIG_OPTIONS="--prefix=/tmp/wx312_opencpn50_macos109" \
-  -DCMAKE_INSTALL_PREFIX=/tmp/opencpn \
+  -DCMAKE_INSTALL_PREFIX= \
   -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 \
+  "/" \
   ..
 make -sj2
 make package
@@ -42,10 +45,5 @@ make package
 wget -q http://opencpn.navnux.org/build_deps/Packages.dmg
 hdiutil attach Packages.dmg
 sudo installer -pkg "/Volumes/Packages 1.2.5/Install Packages.pkg" -target "/"
-make install
 make create-pkg
 
-# Install cludsmith-cli, used in upload.
-sudo -H python3 -m ensurepip
-sudo -H python3 -m pip install -q setuptools
-sudo -H python3 -m pip install -q cloudsmith-cli
