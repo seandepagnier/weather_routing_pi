84bda81cce8451ba3f9ddaa1d5b78aa4bd2b7c83
jongough <jon.gough@eclipsesystems.com.au>
2021-04-12 08:41:09 +1000

    Update to match new macos build enviornment in circleci

--- ci/circleci-build-macos.sh
+++ ci/circleci-build-macos.sh
@@ -4,38 +4,43 @@
 # Build the  MacOS artifacts
 #
 
-# Fix broken ruby on the CircleCI image:
-if [ -n "$CI" ]; then
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-fi
-
 set -xe
 
 set -o pipefail
-for pkg in cairo cmake libarchive libexif wget; do
-    brew list $pkg 2>&1 >/dev/null || brew install $pkg 2>&1 >/dev/null || brew upgrade $pkg
-done
-brew list python@2 2>&1 >/dev/null && brew unlink python@2
-brew reinstall python3
+# Check if the cache is with us. If not, re-install brew.
+brew list --versions libexif || brew update-reset
 
-wget -q http://opencpn.navnux.org/build_deps/wx312_opencpn50_macos109.tar.xz
-tar xJf wx312_opencpn50_macos109.tar.xz -C /tmp
+for pkg in cairo cmake gettext libarchive libexif python wget; do
+    brew list --versions $pkg || brew install $pkg || brew install $pkg || :
+    brew link --overwrite $pkg || brew install $pkg
+done
+
+#wget -q http://opencpn.navnux.org/build_deps/wx312_opencpn50_macos109.tar.xz - unsupported link, replaced with below
+curl -o wx312B_opencpn50_macos109.tar.xz https://download.opencpn.org/s/rwoCNGzx6G34tbC/download
+tar xJf wx312B_opencpn50_macos109.tar.xz -C /tmp
 export PATH="/usr/local/opt/gettext/bin:$PATH"
 echo 'export PATH="/usr/local/opt/gettext/bin:$PATH"' >> ~/.bash_profile
 
+export MACOSX_DEPLOYMENT_TARGET=10.9
+
+# use brew to get Packages.pkg
+if brew list --cask --versions packages; then
+    version=$(pkg_version packages '--cask')
+    sudo installer \
+        -pkg /usr/local/Caskroom/packages/$version/packages/Packages.pkg \
+        -target /
+else
+    brew install --cask packages
+fi
+
 rm -rf build && mkdir build && cd build
 cmake \
-  -DwxWidgets_CONFIG_EXECUTABLE=/tmp/wx312_opencpn50_macos109/bin/wx-config \
-  -DwxWidgets_CONFIG_OPTIONS="--prefix=/tmp/wx312_opencpn50_macos109" \
+  -DwxWidgets_CONFIG_EXECUTABLE=/tmp/wx312B_opencpn50_macos109/bin/wx-config \
+  -DwxWidgets_CONFIG_OPTIONS="--prefix=/tmp/wx312B_opencpn50_macos109" \
   -DCMAKE_INSTALL_PREFIX= \
   -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 \
   "/" \
   ..
 make -sj2
 make package
-
-wget -q http://opencpn.navnux.org/build_deps/Packages.dmg
-hdiutil attach Packages.dmg
-sudo installer -pkg "/Volumes/Packages 1.2.5/Install Packages.pkg" -target "/"
-make create-pkg
 
